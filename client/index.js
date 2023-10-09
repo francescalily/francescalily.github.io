342b8e7f318c16ffa467b1cf75076da67fa4b0f8
Franki Callard <francescalily@outlook.com>
2023-10-09 21:22:14 +0100

    adding grids

--- client/index.js
+++ client/index.js
@@ -1,19 +1,19 @@
-function showContent(sectionId) {
+// function showContent(sectionId) {
     
-    let sections = document.querySelectorAll('.content-section');
-    for(let section of sections) {
-        section.style.display = 'none';
-    }
+//     let sections = document.querySelectorAll('.content-section');
+//     for(let section of sections) {
+//         section.style.display = 'none';
+//     }
 
     
-    let contentSection = document.getElementById(sectionId);
-    if (contentSection) {
-        contentSection.style.display = 'block';
-    }
+//     let contentSection = document.getElementById(sectionId);
+//     if (contentSection) {
+//         contentSection.style.display = 'block';
+//     }
 
     
-    let p5Sketch = document.getElementById('p5-sketch');
-    if(p5Sketch) {
-        p5Sketch.style.display = 'none';
-    }
-}
+//     let p5Sketch = document.getElementById('p5-sketch');
+//     if(p5Sketch) {
+//         p5Sketch.style.display = 'none';
+//     }
+// }
