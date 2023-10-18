d2bdbdb69d1bcf90cf9293471882980b80bba196
Franki Callard <francescalily@outlook.com>
2023-10-18 10:21:16 +0100

    images

--- client/TouchDesigner/script.js
+++ client/TouchDesigner/script.js
@@ -12,7 +12,9 @@ const parameters = {
   materialColor: "#ffeded",
 };
 
-gui.addColor(parameters, "materialColor");
+gui.addColor(parameters, "materialColor").onChange(() => {
+  material.color.set(parameters.materialColor);
+});
 
 /**
  * Base
@@ -26,12 +28,28 @@ const scene = new THREE.Scene();
 /**
  * Test cube
  */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "rgb(69, 248, 14)" })
-);
-scene.add(cube);
+// const cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: "rgb(69, 248, 14)" })
+// );
+// scene.add(cube);
 
+const material = new THREE.MeshToonMaterial({
+  color: parameters.materialColor,
+});
+
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
+const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
+const mesh3 = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+  material
+);
+
+scene.add(mesh1, mesh2, mesh3);
+
+const directionalLight = new THREE.DirectionalLight("#ffffff", 1);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
 /**
  * Sizes
  */
