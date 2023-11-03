fe4053e5bb67de7cc9951b95244dee98cf5d9075
Franki Callard <francescalily@outlook.com>
2023-11-03 16:20:49 +0000

    adding more generative art

--- client/fullPage/index.js
+++ client/fullPage/index.js
@@ -1,6 +1,8 @@
 const images = [
   "url(../assets/static/ball.png)",
   "url(../assets/static/ny.jpg)",
+  "url(../assets/static/ant.png)",
+  "url(../assets/static/textureOne.png)",
 ];
 
 let currentIndex = 0;
